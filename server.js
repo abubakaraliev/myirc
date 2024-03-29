775d60de5dd31323fa6b64cf7af8f28d8f9aee38
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-03-29 09:45:53 +0100

    feat: simple server node

---
+++ server.js
@@ -0,0 +1,10 @@
+// import the http module
+const http = require('http');
+
+// Create an HTTP server that returns a simple 'Hello World' message for all requests
+const server = http.createServer((req, res) => {
+    res.end('Hello World!');
+});
+
+// Start the server on port 3000
+server.listen(process.env.PORT || 3000);
