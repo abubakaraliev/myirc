6203d5776da9bf9998f56df4bbe70ab089bb10e8
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-04-02 16:14:00 +0200

    feat: tables users, channels, messages

---
+++ myIRC.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS `myIRC`;
+
+USE `myIRC`;
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `username` VARCHAR(45) NOT NULL,
+    `email` VARCHAR(45) NOT NULL,
+    `password` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`username`)
+);
+
+CREATE TABLE IF NOT EXISTS `channels` (
+    `identifier` VARCHAR(45) NOT NULL,
+    `creation_date` DATETIME NOT NULL,
+    PRIMARY KEY (`identifier`)
+);
+
+CREATE TABLE IF NOT EXISTS `messages`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `channel_id` VARCHAR(45) NOT NULL,
+    `sender` VARCHAR(45) NOT NULL,
+    `receiver` VARCHAR(45),
+    `message` TEXT NOT NULL,
+    `date` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`channel_id`) REFERENCES `channels`(`identifier`),
+    FOREIGN KEY (`receiver`) REFERENCES `users`(`username`),
+    FOREIGN KEY (`sender`) REFERENCES `users`(`username`)
+);
