ef6b411ef7f010db73370feaaa8611427104d631
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-04-03 10:48:17 +0200

    fix: foreign key conflict

--- myIRC.sql
+++ myIRC.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS `myIRC`;
 USE `myIRC`;
 
 CREATE TABLE IF NOT EXISTS `Users` (
-    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `username` VARCHAR(45) NOT NULL,
     `email` VARCHAR(45) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS `Users` (
 );
 
 CREATE TABLE IF NOT EXISTS `Channels` (
-    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `identifier` VARCHAR(45) NOT NULL,
     `creation_date` DATETIME NOT NULL,
     `type` VARCHAR(45) NOT NULL,
@@ -21,9 +21,9 @@ CREATE TABLE IF NOT EXISTS `Channels` (
 
 CREATE TABLE IF NOT EXISTS `Messages`(
     `id` INT NOT NULL AUTO_INCREMENT,
-    `channel_id` VARCHAR(45) NOT NULL,
-    `sender_id` VARCHAR(45) NOT NULL,
-    `receiver_id` VARCHAR(45),
+    `channel_id` INTEGER NOT NULL,
+    `sender_id` INTEGER NOT NULL,
+    `receiver_id` INTEGER,
     `text` TEXT NOT NULL,
     `date` DATETIME NOT NULL,
     PRIMARY KEY (`id`),
